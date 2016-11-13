7f70aa27af97083568c5647cc03a68370d59c58a
gg <fountainoy@icloud.com>
2016-11-13 23:36:39 +0800

    PC

--- Find_bound.py
+++ Find_bound.py
@@ -1,15 +1,16 @@
 import cv2
 import numpy as np
 
-img_dir = "dir"
+#img_dir = "dir"
 
-im = cv2.imread()
+#im = cv2.imread()
 
 def get_bound_rects(im):
 
     im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    im_gray = cv2.GaussianBlur(im_gray,(5,5),0)
     ret, im_th = cv2.threshold(im_gray, 90, 255, cv2.THRESH_BINARY_INV)
-    ctrs, hier = cv2.findContours(im_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    _,ctrs, hier = cv2.findContours(im_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     # Get rects contains each contour
     rects = [cv2.boundingRect(ctr) for ctr in ctrs]
     return rects
--- train.py
+++ train.py
@@ -2,7 +2,6 @@ import Deal_Image
 import tensorflow as tf
 from Deal_Image import *
 from Find_bound import *
-import cv
 x_start = 0
 y_start = 0
 half_height = 350
@@ -14,9 +13,10 @@ number_heigh = 28
 
 def train(if_train):
     sess = tf.InteractiveSession()
-    img =  get_image(sess)
-    up_img = crop_digits(sess,img,x_start,y_start,half_height,half_width)
+    up_img = cv2.imread(IMAGE_DIR)
+    #up_img = crop_digits(sess,img,x_start,y_start,half_height,half_width)
     get_bound_rects(up_img)
+    print ("ok")
     #plot_sampleWithLables(img, 1000, sess, tartget_height, target_width)
 
 
